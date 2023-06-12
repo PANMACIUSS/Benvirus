b657a66cd9c6f7bd058259ece0031bec9d0b3ad6
PANMACIUSS <81952915+PANMACIUSS@users.noreply.github.com>
2023-06-12 11:19:49 +0200

    ben3.bat

--- benvirus/ben3.bat
+++ benvirus/ben3.bat
@@ -2,6 +2,6 @@
 :ben3
 nircmd.exe mutesysvolume 0
 nircmd.exe setsysvolume 65535
-nircmd mediaplay 1000 "C:\Users\Admin\Pictures\Camera Roll\pracadomowa\talkingben.mp3"
+nircmd mediaplay 1000 "C:\Users\%USERPROFILE%\Pictures\Benvirus\benvirus\talkingben.mp3"
 timeout /t 122 /nobreak
 goto :ben3
